2e537a1aefcc1686138e697c62a1e1d4e6083e94
olivia <olivia.bergaglia@gmail.com>
2025-10-22 19:33:25 +0200

    fix CI error by comenting home in 'resources/js/layouts/auth/AuthSimpleLayout.vue'

--- resources/js/layouts/auth/AuthSimpleLayout.vue
+++ resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
-import { home } from '@/routes';
+// import { home } from '@/routes';
 import { Link } from '@inertiajs/vue3';
 
 defineProps<{
